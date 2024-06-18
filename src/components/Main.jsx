195a945f5c6ecc4805384f424728bf7eae681389
Meet Bhesaniya <meetbhesaniya191@gmail.com>
2024-06-18 17:22:42 +0530

    made change to quality of image and added a title

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ const Main = (props) => {
 
   return (
     <div className=" absolute h-[100%] w-full">
-      <img src={data?.url} alt={data.title || "bg_img"}  className="h-full md:w-full lg:w-full object-cover" />
+      <img src={data?.hdurl} alt={data.title || "bg_img"}  className="h-full md:w-full lg:w-full object-cover" />
     </div>
   );
 };
